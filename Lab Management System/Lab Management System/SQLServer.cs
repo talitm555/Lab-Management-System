76a55292ebf94730947186ea76867474ce575469
Talit Mahmood <talitm555@gmail.com>
2019-03-14 01:31:47 +0500

    Added DB Connectivity File.

---
+++ Lab Management System/Lab Management System/SQLServer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+using System.Data.Sql;
+
+namespace Lab_Management_System
+{
+    class SQLServer
+    {
+        public static string conURL = "Data Source = TALITMAHMOOD-PC; Initial Catalog = ProjectB; Integrated Security = True; MultipleActiveResultSets = True";
+        public static SqlConnection Connection = new SqlConnection(conURL);
+    }
+}
