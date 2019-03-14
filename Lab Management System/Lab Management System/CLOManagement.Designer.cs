34f42da8d32a46b74a3096b8f32bc050da94e1e3
Talit Mahmood <talitm555@gmail.com>
2019-03-15 00:58:16 +0500

    CLO Management Module Completed.

--- Lab Management System/Lab Management System/CLOManagement.Designer.cs
+++ Lab Management System/Lab Management System/CLOManagement.Designer.cs
@@ -28,19 +28,177 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.txt_CLOName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_CLO = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.projectBDataSetCLO = new Lab_Management_System.ProjectBDataSetCLO();
+            this.cloBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.cloTableAdapter = new Lab_Management_System.ProjectBDataSetCLOTableAdapters.CloTableAdapter();
+            this.btn_Clear = new System.Windows.Forms.Button();
+            this.idDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dateCreatedDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dateUpdatedDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Update = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.Delete = new System.Windows.Forms.DataGridViewButtonColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.projectBDataSetCLO)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cloBindingSource)).BeginInit();
             this.SuspendLayout();
+            // 
+            // txt_CLOName
+            // 
+            this.txt_CLOName.Location = new System.Drawing.Point(255, 38);
+            this.txt_CLOName.Name = "txt_CLOName";
+            this.txt_CLOName.Size = new System.Drawing.Size(217, 20);
+            this.txt_CLOName.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(171, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "CLO Name:";
+            // 
+            // btn_CLO
+            // 
+            this.btn_CLO.Location = new System.Drawing.Point(255, 64);
+            this.btn_CLO.Name = "btn_CLO";
+            this.btn_CLO.Size = new System.Drawing.Size(127, 23);
+            this.btn_CLO.TabIndex = 2;
+            this.btn_CLO.Text = "Add CLO";
+            this.btn_CLO.UseVisualStyleBackColor = true;
+            this.btn_CLO.Click += new System.EventHandler(this.btn_CLO_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.idDataGridViewTextBoxColumn,
+            this.nameDataGridViewTextBoxColumn,
+            this.dateCreatedDataGridViewTextBoxColumn,
+            this.dateUpdatedDataGridViewTextBoxColumn,
+            this.Update,
+            this.Delete});
+            this.dataGridView1.DataSource = this.cloBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 134);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(695, 165);
+            this.dataGridView1.TabIndex = 3;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // projectBDataSetCLO
+            // 
+            this.projectBDataSetCLO.DataSetName = "ProjectBDataSetCLO";
+            this.projectBDataSetCLO.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // cloBindingSource
+            // 
+            this.cloBindingSource.DataMember = "Clo";
+            this.cloBindingSource.DataSource = this.projectBDataSetCLO;
+            // 
+            // cloTableAdapter
+            // 
+            this.cloTableAdapter.ClearBeforeFill = true;
+            // 
+            // btn_Clear
+            // 
+            this.btn_Clear.Location = new System.Drawing.Point(397, 64);
+            this.btn_Clear.Name = "btn_Clear";
+            this.btn_Clear.Size = new System.Drawing.Size(75, 23);
+            this.btn_Clear.TabIndex = 15;
+            this.btn_Clear.Text = "Clear";
+            this.btn_Clear.UseVisualStyleBackColor = true;
+            this.btn_Clear.Click += new System.EventHandler(this.btn_Clear_Click);
+            // 
+            // idDataGridViewTextBoxColumn
+            // 
+            this.idDataGridViewTextBoxColumn.DataPropertyName = "Id";
+            this.idDataGridViewTextBoxColumn.HeaderText = "CLO ID";
+            this.idDataGridViewTextBoxColumn.Name = "idDataGridViewTextBoxColumn";
+            this.idDataGridViewTextBoxColumn.ReadOnly = true;
+            this.idDataGridViewTextBoxColumn.Width = 50;
+            // 
+            // nameDataGridViewTextBoxColumn
+            // 
+            this.nameDataGridViewTextBoxColumn.DataPropertyName = "Name";
+            this.nameDataGridViewTextBoxColumn.HeaderText = "CLO Name";
+            this.nameDataGridViewTextBoxColumn.Name = "nameDataGridViewTextBoxColumn";
+            this.nameDataGridViewTextBoxColumn.Width = 150;
+            // 
+            // dateCreatedDataGridViewTextBoxColumn
+            // 
+            this.dateCreatedDataGridViewTextBoxColumn.DataPropertyName = "DateCreated";
+            this.dateCreatedDataGridViewTextBoxColumn.HeaderText = "Date Created";
+            this.dateCreatedDataGridViewTextBoxColumn.Name = "dateCreatedDataGridViewTextBoxColumn";
+            this.dateCreatedDataGridViewTextBoxColumn.Width = 150;
+            // 
+            // dateUpdatedDataGridViewTextBoxColumn
+            // 
+            this.dateUpdatedDataGridViewTextBoxColumn.DataPropertyName = "DateUpdated";
+            this.dateUpdatedDataGridViewTextBoxColumn.HeaderText = "Date Updated";
+            this.dateUpdatedDataGridViewTextBoxColumn.Name = "dateUpdatedDataGridViewTextBoxColumn";
+            this.dateUpdatedDataGridViewTextBoxColumn.Width = 150;
+            // 
+            // Update
+            // 
+            this.Update.HeaderText = "Update";
+            this.Update.Name = "Update";
+            this.Update.ReadOnly = true;
+            this.Update.Text = "Edit";
+            this.Update.UseColumnTextForButtonValue = true;
+            this.Update.Width = 75;
+            // 
+            // Delete
+            // 
+            this.Delete.HeaderText = "Delete";
+            this.Delete.Name = "Delete";
+            this.Delete.ReadOnly = true;
+            this.Delete.Text = "Delete";
+            this.Delete.UseColumnTextForButtonValue = true;
+            this.Delete.Width = 75;
             // 
             // CLOManagement
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(715, 311);
+            this.Controls.Add(this.btn_Clear);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btn_CLO);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_CLOName);
             this.Name = "CLOManagement";
             this.Text = "CLO Management";
+            this.Load += new System.EventHandler(this.CLOManagement_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.projectBDataSetCLO)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cloBindingSource)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txt_CLOName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_CLO;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private ProjectBDataSetCLO projectBDataSetCLO;
+        private System.Windows.Forms.BindingSource cloBindingSource;
+        private ProjectBDataSetCLOTableAdapters.CloTableAdapter cloTableAdapter;
+        private System.Windows.Forms.Button btn_Clear;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dateCreatedDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dateUpdatedDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewButtonColumn Update;
+        private System.Windows.Forms.DataGridViewButtonColumn Delete;
     }
 }
--- Lab Management System/Lab Management System/CLOManagement.cs
+++ Lab Management System/Lab Management System/CLOManagement.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,110 @@ namespace Lab_Management_System
 {
     public partial class CLOManagement : Form
     {
+        public int id;
+        public void ShowCLOData()
+        {
+           // SQLServer.Connection.Open();
+            string cmd = "SELECT * FROM [ProjectB].[dbo].[CLO]";
+            SqlCommand command = new SqlCommand(cmd, SQLServer.Connection);
+            // Add the parameters if required
+            command.Parameters.Add(new SqlParameter("0", 1));
+            SqlDataReader reader = command.ExecuteReader();
+            DataTable dt = new DataTable();
+            dt.Load(reader);
+            dataGridView1.DataSource = dt;
+        }
         public CLOManagement()
         {
             InitializeComponent();
+        }
+
+        private void CLOManagement_Load(object sender, EventArgs e)
+        {
+            
+            SQLServer.Connection.Open();
+            ShowCLOData();
+            // TODO: This line of code loads data into the 'projectBDataSetCLO.Clo' table. You can move, or remove it, as needed.
+            this.cloTableAdapter.Fill(this.projectBDataSetCLO.Clo);
+
+        }
+
+        private void btn_Clear_Click(object sender, EventArgs e)
+        {
+            txt_CLOName.Clear();
+        }
+
+        private void btn_CLO_Click(object sender, EventArgs e)
+        {
+            if (btn_CLO.Text == "Add CLO")
+            {
+                string query = "INSERT INTO [ProjectB].[dbo].[CLO] (Name, DateCreated, DateUpdated) VALUES ('"+ txt_CLOName.Text +"', '"+ DateTime.Now+ "', '" + DateTime.Now + "')";
+                using (SqlCommand command = new SqlCommand(query, SQLServer.Connection))
+                {
+                    int result = command.ExecuteNonQuery();
+
+                    // Checking for Errors
+                    if (result < 0) MessageBox.Show("An Error occured while adding CLO into DB!");
+                    if (result > 0)
+                    {
+                        MessageBox.Show("CLO Added Successfully!");
+                        txt_CLOName.Clear();
+                        ShowCLOData();
+                    }
+                }
+
+            }
+            else if (btn_CLO.Text == "Update CLO")
+            {
+                string query = "UPDATE [ProjectB].[dbo].[CLO] SET Name = '" + txt_CLOName.Text + "', DateUpdated = '" + DateTime.Now + "' WHERE id = '"+id+"'";
+                using (SqlCommand command = new SqlCommand(query, SQLServer.Connection))
+                {
+                    int result = command.ExecuteNonQuery();
+
+                    // Checking for Errors
+                    if (result < 0) MessageBox.Show("An Error occured while updating CLO into DB!");
+                    if (result > 0)
+                    {
+                        MessageBox.Show("CLO Updated Successfully!");
+                        btn_CLO.Text = "Add CLO";
+                        txt_CLOName.Clear();
+                        ShowCLOData();
+                    }
+                }
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            var senderGrid = (DataGridView)sender;
+
+            if (senderGrid.Columns[e.ColumnIndex] is DataGridViewButtonColumn &&
+                e.RowIndex >= 0 && senderGrid.Columns[e.ColumnIndex].HeaderText == "Update")
+            {
+                id = Convert.ToInt32(dataGridView1[0, e.RowIndex].Value);
+                txt_CLOName.Text = Convert.ToString(dataGridView1[1, e.RowIndex].Value);
+                btn_CLO.Text = "Update CLO";
+            }
+            else if (senderGrid.Columns[e.ColumnIndex] is DataGridViewButtonColumn &&
+               e.RowIndex >= 0 && senderGrid.Columns[e.ColumnIndex].HeaderText == "Delete")
+            {
+
+                id = Convert.ToInt32(dataGridView1[0, e.RowIndex].Value);
+                string query = "DELETE FROM [ProjectB].[dbo].[CLO] Where id='"+id+"'";
+                using (SqlCommand command = new SqlCommand(query, SQLServer.Connection))
+                {
+                    int result = command.ExecuteNonQuery();
+
+                    // Checking for Errors
+                    if (result < 0) MessageBox.Show("An Error occured while Deleting CLO from DB!");
+                    if (result > 0)
+                    {
+                        MessageBox.Show("CLO Deleted Successfully!");
+                        ShowCLOData();
+                    }
+
+                }
+            }
         }
     }
 }
